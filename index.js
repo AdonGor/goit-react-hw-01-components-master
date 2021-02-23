169744acb492b26c2aa88ae070c629b0be395f00
Sergij <AdonGor@users.noreply.github.com>
2021-02-23 08:25:09 +0200

    ...

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/App';
+import 'modern-normalize/modern-normalize.css';
+import "./style.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
